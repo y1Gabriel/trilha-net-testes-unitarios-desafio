608381b3a58f1326c3b0626dbf0dd521ce8b51bb
Gabriel Theodoro <gabriel.theo@hotmail.com>
2024-01-05 20:52:19 -0300

    Desafio finalizad!

--- TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
@@ -44,12 +44,15 @@ public class ValidacoesListaTests
         var numeroParaProcurar = 10;
 
         // Act
+        var resultado = _validacoes.ListaContemDeterminadoNumero(lista, numeroParaProcurar);
 
         // Assert
-    }
+        Assert.False(resultado);
+    }   
 
     //TODO: Corrigir a anotação [Fact]
-    public void DeveMultiplicarOsElementosDaListaPor2()
+    [Fact]
+    public void DeveMultiplicarOsNumerosDaListaPor2()
     {
         //TODO: Implementar método de teste
 
@@ -58,9 +61,11 @@ public class ValidacoesListaTests
         var resultadoEsperado = new List<int> { 10, 14, 16, 18 };
         
         // Act
+        var resultado = _validacoes.MultiplicarNumerosLista(lista, 2);
 
         // Assert
-    }
+        Assert.Equal(resultadoEsperado, lista);
+    }   
 
     [Fact]
     public void DeveRetornar9ComoMaiorNumeroDaLista()
@@ -69,12 +74,14 @@ public class ValidacoesListaTests
 
         // Arrange
         var lista = new List<int> { 5, -1, -8, 9 };
+        var resultadoEsperado = new List<int> { 9, 5, -1, -9 };
 
         // Act
+        var resultado = _validacoes.RetornarMaiorNumeroLista(resultadoEsperado);
 
         // Assert
         //TODO: Corrigir o Assert.Equal com base no retorno da chamada ao método
-        Assert.Equal(9, 9);
+        Assert.Equal(9, resultado);
     }
 
     [Fact]
@@ -84,12 +91,13 @@ public class ValidacoesListaTests
 
         // Arrange
         var lista = new List<int> { 5, -1, -8, 9 };
+        var resultadoEsperado = new List<int> { 9, 5, -1, -8 };
 
         // Act
         var resultado = _validacoes.RetornarMenorNumeroLista(lista);
 
         // Assert
         //TODO: Corrigir o Assert.Equal com base no retorno da chamada ao método
-        Assert.Equal(-8, -8);
+        Assert.Equal(-8, resultado);
     }
 }
